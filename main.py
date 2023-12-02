8740ce6ee94fdc16f3ade0d3dc97705235231bfa
Anton Lobanov <antnlobanov@gmail.com>
2023-12-03 02:41:47 +0500

    HTTPS support and fix conflicts

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Response, status, Depends
 from sqlalchemy.orm import Session
 from app.auth import hash, jwt_handler
-from app.data import crud, models, schemas
+from app.data import crud, schemas
 from app.data.database import session, engine, base
-import re
+import re, uvicorn
 
 base.metadata.create_all(engine)
 
-app = FastAPI()
+api = FastAPI()
 
 def get_db():
     db = session()
@@ -16,7 +16,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/api/login")
+@api.post("/api/login")
 async def login(response: Response, auth: schemas.AuthCreate, db: Session = Depends(get_db)):
     if not crud.get_user_by_email(db, auth.email):
         response.status_code = status.HTTP_401_UNAUTHORIZED
@@ -27,7 +27,7 @@ async def login(response: Response, auth: schemas.AuthCreate, db: Session = Depe
     return {"access_token": jwt_handler.access_token(crud.get_id(db, auth.email)),
             "refresh_token": jwt_handler.refresh_token(crud.get_id(db, auth.email))}
 
-@app.post("/api/register", status_code=status.HTTP_201_CREATED)
+@api.post("/api/register", status_code=status.HTTP_201_CREATED)
 async def register(response: Response, profile: schemas.ProfileCreate, auth: schemas.AuthCreate, db: Session = Depends(get_db)):
     if not re.match(r"\S+@\S+\.\S+" , auth.email):
         response.status_code = status.HTTP_400_BAD_REQUEST
@@ -41,3 +41,12 @@ async def register(response: Response, profile: schemas.ProfileCreate, auth: sch
     res = crud.create_profile(db, profile)
     crud.create_auth(db, auth, res)
     return {"code": "success"}
+
+if __name__ == '__main__':
+    uvicorn.run("main:api",
+                host="0.0.0.0",
+                port=443,
+                reload=True,
+                ssl_keyfile="./novatorsmobile_ru.key", 
+                ssl_certfile="./novatorsmobile_ru.full.crt"
+                )
